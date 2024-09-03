3cc6adb1a942eea4525ccf904859d41c7b661ea7
Hein Wai Yan Htet <heinwaiyanhtet2020@gmail.com>
2024-09-03 23:29:13 +0630

    lambda with prisma

--- lambda/index.mjs
+++ lambda/index.mjs
@@ -1,12 +1,16 @@
-export const handler = async (event) => {
+export const handler = async (event) =>
+{
   console.log('Received event:', event);
 
-  // Process the request based on the field name
   if (event.field === 'getGreeting') {
     const { name } = event.arguments;
     return `Hello, ${name || 'World'}!`;
   }
 
-  // Handle unknown fields
   return `Unknown field: ${event.field}`;
+  
 };
+
+
+
+
